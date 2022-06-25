359ed5949ef19895bc060945f7973ef974dcddb3
berker <demirerberker@gmail.com>
2022-06-25 20:40:56 +0300

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.svelte'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		fontFamily: {
